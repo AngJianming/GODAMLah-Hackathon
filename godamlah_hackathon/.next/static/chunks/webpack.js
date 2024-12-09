c03cbe5d208cb10a51f144fa51a56a080306103f
Dick18643 <dickson18643@gmail.com>
2024-12-09 11:04:18 +0800

    set up next js

--- godamlah_hackathon/.next/static/chunks/webpack.js
+++ godamlah_hackathon/.next/static/chunks/webpack.js
@@ -89,6 +89,18 @@
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/create fake namespace object */
 /******/ 	(() => {
 /******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
@@ -178,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7e2e291998198624")
+/******/ 		__webpack_require__.h = () => ("d27aaf2b92dc43a6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
