926b6aa4f39c0ccc6cc3948f25203694daee5d14
wz-Tan <wenzhe1423@gmail.com>
2024-12-10 10:46:01 +0800

    AuthCode (Not Completed)

---
+++ authtest.py
@@ -0,0 +1,36 @@
+import os 
+from supabase import Client,create_client
+
+#Environ for system wide access
+os.environ["supabase_url"]="https://lhnoptcoelsmxxobzewx.supabase.co"
+os.environ["supabase_key"]="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxobm9wdGNvZWxzbXh4b2J6ZXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4OTQyNzMsImV4cCI6MjA0NjQ3MDI3M30.M6KWgmvB8BIaPH2zuJ382hP1tWMwnNcEYWk3TLw4X1U"
+
+url:str=os.environ.get("supabase_url")
+key:str=os.environ.get("supabase_key")
+supabase:Client=create_client(url,key)
+
+#password should be string
+#After this part, it will send a confirmation email. (needs improvement)
+def register(username,email,password):
+    response=supabase.auth.sign_up(
+        {
+            "username":username,
+            "email":email,
+            "password":password
+        }
+    )
+    return response
+    
+def sign_in(email,password):
+    response=supabase.auth.sign_in_with_password(
+        {
+            "email":email,
+            "password":password
+        }
+    )
+    return response
+
+def sign_out():
+    response=supabase.auth.sign_out
+    
+print(sign_in("wenzhe1423@gmail.com","123456"))
